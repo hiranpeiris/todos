edbd1f343712c4a1290184e357f78660fbd5dcaf
Hiran Peiris <hiranpeiris.sl@gmail.com>
2019-09-14 07:53:40 +0530

    Init

---
+++ src/components/Todos.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Todos extends Component {
+
+    render() {
+        return (
+            <div>
+                <h4>Todos</h4>
+            </div>
+        );
+    }
+}
+
+export default Todos;
